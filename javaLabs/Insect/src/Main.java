bf282494cf05b43c1f264ac3a23855159426df5e
VolodiaGHz <VolodiaGHz@users.github.com>
2018-06-10 15:28:25 +0300

    1 lab with fixed code

--- javaLabs/Insect/src/Main.java
+++ javaLabs/Insect/src/Main.java
@@ -2,27 +2,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        Insect Ladybag = new Insect();
-        Insect Mantis = new Insect("Mantis", 3, 2, 0.133);
-        Insect Bee = new Insect("Bee", 6, 4, 0.150, 150000);
+        Insect ladybag = new Insect();
+        Insect mantis = new Insect("Mantis", 3, 2, 0.133);
+        Insect bee = new Insect("Bee", 6, 4, 0.150, 150000);
 
-        System.out.println(Ladybag.toString());
-        System.out.println(Mantis.toString());
-        System.out.println(Bee.toString());
-
-        Insect.printStaticSum();
-        Bee.printSum();
-
-        Ladybag.resetValues("Fly", 6, 4, 0.050, 20000);
-        Mantis.resetValues("DragonFly", 8, 6, 0.200, 1500);
-        Bee.resetValues("Buterfly", 4, 2, 0.190, 500);
-
-        System.out.println(Ladybag.toString());
-        System.out.println(Mantis.toString());
-        System.out.println(Bee.toString());
+        System.out.println(ladybag.toString());
+        System.out.println(mantis.toString());
+        System.out.println(bee.toString());
 
         Insect.printStaticSum();
-        Ladybag.printSum();
+        bee.printSum();
+
+        ladybag.resetValues("Fly", 6, 4, 0.050, 20000);
+        mantis.resetValues("DragonFly", 8, 6, 0.200, 1500);
+        bee.resetValues("Buterfly", 4, 2, 0.190, 500);
+
+        System.out.println(ladybag.toString());
+        System.out.println(mantis.toString());
+        System.out.println(bee.toString());
+
+        Insect.printStaticSum();
+        ladybag.printSum();
 
 
     }
